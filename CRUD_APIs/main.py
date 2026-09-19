13ccf3e415e92f7c52af8ca75d077821642cf307
pminhtaiii <taipham2006711@gmail.com>
2026-09-19 10:44:21 +0700

    Stage 5: Swagger UI

--- CRUD_APIs/main.py
+++ CRUD_APIs/main.py
@@ -43,11 +43,19 @@ def health():
         "status": "ok"
     }
     
-@app.get("/tasks")
+@app.get(
+    "/tasks",
+    summary="Get all the tasks available",
+    description="Return all tasks in memory"
+)
 def get_tasks():
     return tasks
 
-@app.get("/tasks/{task_id}")
+@app.get(
+    "/tasks/{task_id}",
+    summary="Get one task based on task_id",
+    description="Return a task by its task id"
+)
 def get_task(task_id:int):
     for task in tasks:
         if task["id"] == task_id:
@@ -57,7 +65,11 @@ def get_task(task_id:int):
         content={"error": f"Task {task_id} not found"}
     )
     
-@app.post("/tasks", status_code=201)
+@app.post(
+    "/tasks", status_code=201,
+    summary="Create a new task",
+    description="Create a new task with a new id, return 404 if the title is None or an empty string"
+)
 def create_task(task_data: CreateTask):
     if task_data.title is None or task_data.title.strip() == "":
         return JSONResponse(
@@ -73,7 +85,11 @@ def create_task(task_data: CreateTask):
     tasks.append(new_task)
     return new_task
 
-@app.put("/tasks/{task_id}")
+@app.put(
+    "/tasks/{task_id}",
+    summary="Update a task",
+    description="Update a task with a new payload"
+)
 def update_task(task_id: int, task_data: UpdateTask):
     if task_data.title is None and task_data.done is None:
         return JSONResponse(
