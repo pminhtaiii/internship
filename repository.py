5b3c674322145ebd7b1689b14c4e4ec4cbe6b0e5
Tài Phạm Minh <taipham2006711@gmail.com>
2026-09-21 00:14:57 +0700

    Stage 1: connect via .env and create table

---
+++ repository.py
@@ -0,0 +1,40 @@
+import os
+import psycopg
+from psycopg.rows import dict_row
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+def get_postgres_connection():
+    return psycopg.connect(DATABASE_URL, row_factory=dict_row)
+
+def init_postgres():
+    conn = get_postgres_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks(
+            id SERIAL PRIMARY KEY,
+            title TEXT NOT NULL,
+            done BOOLEAN NOT NULL DEFAULT FALSE
+        )
+    """)
+    
+    cursor.execute("SELECT COUNT(*) FROM tasks")
+    count = cursor.fetchone()[0]
+    
+    if count == 0:
+        cursor.executemany(
+            "INSERT INTO tasks (title, done) VALUES (?, ?)",
+            [
+                ("learn new knowledge", True),
+                ("get a job", False),
+                ("become successful", False)
+            ]
+        )
+    
+    conn.commit()
+    conn.close()
+    cursor.close()
